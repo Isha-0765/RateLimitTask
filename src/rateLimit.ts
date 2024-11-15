339a5f1abd2344598e93c77bbadad1068ae5c726
Isha <ishaa.0134@gmail.com>
2024-11-15 12:54:44 +0530

    redis

--- src/rateLimit.ts
+++ src/rateLimit.ts
@@ -1,43 +1,34 @@
 import { Request, Response, NextFunction } from 'express';
 import { client } from './redisClient';
 
-const MAX_REQUESTS = 10;
-const WINDOW_TIME = 60 * 1000;
-interface UserRequestData {
-  tokens: number;
-  lastRequestTime: number;
-}
+const MAX_REQUESTS = 10; 
+const WINDOW_TIME = 60; 
 
-const userRequests: { [key: string]: UserRequestData } = {};
+export const rateLimit = async (req: Request, res: Response, next: NextFunction) => {
+  const userIP = req.ip || '';
 
-export const rateLimit = (req: Request, res: Response, next: NextFunction) => {
-  const userIP:string = req.ip || '';
-  const currentTime = Date.now();
+  try {
+    const userData = await client.get(userIP);
 
-  if (!userRequests[userIP]) {
-    userRequests[userIP] = {
-      tokens: MAX_REQUESTS,
-      lastRequestTime: currentTime,
-    };
-  }
+    if (!userData) {
+      await client.set(userIP, MAX_REQUESTS - 1, {
+        EX: WINDOW_TIME,
+      });
+      next();
+    } else {
+      let tokens = parseInt(userData, 10);
 
-  const userData = userRequests[userIP];
-  const timeElapsed = currentTime - userData.lastRequestTime;
-
-  if (timeElapsed > WINDOW_TIME) {
-    userData.tokens = MAX_REQUESTS;
-    userData.lastRequestTime = currentTime;
-  } else {
-    const tokensToAdd = Math.floor((timeElapsed / WINDOW_TIME) * MAX_REQUESTS);
-    userData.tokens = Math.min(MAX_REQUESTS, userData.tokens + tokensToAdd);
-    userData.lastRequestTime = currentTime;
-  }
-
-  if (userData.tokens > 0) {
-    userData.tokens -= 1;
-    next(); 
-  } else {
-    console.log(`Request from ${userIP} denied. Rate limit exceeded.`);
-    res.status(429).json({ error: "Too Many Requests - try again later." });
+      if (tokens > 0) {
+        tokens -= 1;
+        await client.set(userIP, tokens);
+        next();
+      } else {
+        console.log(`Request from ${userIP} denied. Rate limit exceeded.`);
+        res.status(429).json({ error: 'Too Many Requests - try again later.' });
+      }
+    }
+  } catch (error) {
+    console.error('Redis error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
